24b69adf66a2d134b5359853134d8fb4b18ce7f5
sudakovcom <ilyasudakov@icloud.com>
2023-01-19 19:43:00 +0300

    all

--- main.py
+++ main.py
@@ -82,6 +82,39 @@ def Moore_Skelboe(e, interval_d):
     return list_of_intervals[0][1]
 
 
+# def Moore_Skelboe(f, index, D, p):
+#     e = 0.001
+#     interval_d = D.copy()
+#     for i in range(len(p)):
+#         interval_d[i] = interval[p[i], p[i]]
+#     interval_d[index] = D[index]
+#
+#     interval_e = f(interval_d)
+#     list_of_intervals = [[interval_d, interval_e]]
+#     U = fpu.max(interval_e)[1]
+#     w = len(interval_e)
+#     best_interval = list_of_intervals[0]
+#     while w > e:
+#         list_of_intervals.pop(0)
+#         mid = (fpu.max(best_interval[0][index])[0] + fpu.max(best_interval[0][index])[1]) / 2
+#         interval_1 = best_interval[0].copy()
+#         interval_2 = best_interval[0].copy()
+#         interval_1[index] = interval[fpu.max(best_interval[0][index])[0], mid]
+#         interval_1e = f(interval_1)
+#         interval_2[index] = interval[mid, fpu.max(best_interval[0][index])[1]]
+#         interval_2e = f(interval_2)
+#         U = min(U, fpu.max(interval_1e)[1])
+#         U = min(U, fpu.max(interval_2e)[1])
+#         list_of_intervals.append([interval_1, interval_1e])
+#         list_of_intervals.append([interval_2, interval_2e])
+#         for el in list_of_intervals:
+#             if U < fpu.max(el[1])[0]:
+#                 list_of_intervals.remove(el)
+#         list_of_intervals.sort(key=lambda item: fpu.max(item[1])[0])
+#         best_interval = list_of_intervals[0]
+#         w = fpu.max(best_interval[0][index])[1] - fpu.max(best_interval[0][index])[0]
+#     return fpu.max(list_of_intervals[0][1])[0]
+
 a = interval[-1, 1]
 b = interval[-1, 1]
 print(Multidimensional_Moore_Skelboe(0.0001, [a, b]))
