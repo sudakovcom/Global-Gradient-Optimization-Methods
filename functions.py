133c356f66455f9bde7ff957b43d7b5f1b743bca
sudakovcom <ilyasudakov@icloud.com>
2023-02-05 00:58:16 +0300

    d

--- functions.py
+++ functions.py
@@ -4,24 +4,27 @@ from interval import fpu
 from interval import interval
 
 
+# просто функция
 def F_0(x):
+    return (x[0] ** 2) / 2 - (x[1] ** 2) / 4 + 3
+
+
+def F_0_i(x):
+    return (x[0] ** 2) / 2 - (x[1] ** 2) / 4 + 3
+
+
+# Функция Экли
+def F_1(x):
     return -20 * math.exp(-0.2 * math.sqrt(0.5 * (x[0] * x[0] + x[1] * x[1]))) - math.exp(
         0.5 * (math.cos(2 * math.pi * x[0]) + math.cos(2 * math.pi * x[1]))) + math.exp(1) + 20
 
 
-def F_0_i(x):
+def F_1_i(x):
     return -20 * imath.exp(-0.2 * imath.sqrt(0.5 * (x[0] * x[0] + x[1] * x[1]))) - imath.exp(
         0.5 * (imath.cos(2 * math.pi * x[0]) + imath.cos(2 * math.pi * x[1]))) + math.exp(1) + 20
 
 
-def F_1(x):
-    return (x[0] ** 2) / 2 - (x[1] ** 2) / 4 + 3
-
-
-def F_1_i(x):
-    return (x[0] ** 2) / 2 - (x[1] ** 2) / 4 + 3
-
-
+# Функция Била
 def F_2(x):
     return (1.5 - x[0] + x[0] * x[1]) ** 2 + (2.25 - x[0] + x[0] * x[1] ** 2) ** 2 + (2.625 - x[0] + x[0] * x[1] ** 3) ** 2
 
@@ -30,6 +33,7 @@ def F_2_i(x):
     return (1.5 - x[0] + x[0] * x[1]) ** 2 + (2.25 - x[0] + x[0] * x[1] ** 2) ** 2 + (2.625 - x[0] + x[0] * x[1] ** 3) ** 2
 
 
+# Функция Бута
 def F_3(x):
     return (x[0] + 2 * x[1] - 7) ** 2 + (2 * x[0] + x[1] - 5) ** 2
 
@@ -38,6 +42,7 @@ def F_3_i(x):
     return (x[0] + 2 * x[1] - 7) ** 2 + (2 * x[0] + x[1] - 5) ** 2
 
 
+# Функция Матьяса
 def F_4(x):
     return 0.26 * (x[0] ** 2 + x[1] ** 2) - 0.48 * x[0] * x[1]
 
@@ -46,4 +51,122 @@ def F_4_i(x):
     return 0.26 * (x[0] ** 2 + x[1] ** 2) - 0.48 * x[0] * x[1]
 
 
-Functions_1 = [F_0, F_0_i, F_1, F_1_i, F_2, F_2_i, F_3, F_3_i, F_4, F_4_i]
+# Функция Растригина
+def F_5(x):
+    n = len(x)
+    val = 10 * n
+    for i in range(n):
+        val += x[i] ** 2 - 10 * math.cos(2 * math.pi * x[i])
+    return val
+
+
+def F_5_i(x):
+    n = len(x)
+    val = 10 * n
+    for i in range(n):
+        val += x[i] ** 2 - 10 * imath.cos(2 * math.pi * x[i])
+    return val
+
+
+# Функция Сферы
+def F_6(x):
+    val = 0
+    n = len(x)
+    for i in range(n):
+        val += x[i] ** 2
+    return val
+
+
+def F_6_i(x):
+    val = 0
+    n = len(x)
+    for i in range(n):
+        val += x[i] ** 2
+    return val
+
+
+# Функция Розенброка
+def F_7(x):
+    val = 0
+    n = len(x)
+    for i in range(n - 1):
+        val += 100 * (x[i + 1] - x[i] ** 2) ** 2 + (x[i] - 1) ** 2
+    return val
+
+
+def F_7_i(x):
+    val = 0
+    n = len(x)
+    for i in range(n - 1):
+        val += 100 * (x[i + 1] - x[i] ** 2) ** 2 + (x[i] - 1) ** 2
+    return val
+
+
+# Функция Леви
+def F_8(x):
+    return math.sin(3 * math.pi * x[0]) ** 2 + (x - 1) ** 2 * (1 + math.sin(3 * math.pi * x[1]) ** 2) + (x[1] - 1) ** 2 * (1 + math.sin(
+        2 * math.pi * x[1]) ** 2)
+
+
+def F_8_i(x):
+    return imath.sin(3 * math.pi * x[0]) ** 2 + (x - 1) ** 2 * (1 + imath.sin(3 * math.pi * x[1]) ** 2) + (x[1] - 1) ** 2 * (1 + imath.sin(
+        2 * math.pi * x[1]) ** 2)
+
+
+# Функция Химмельблау
+def F_9(x):
+    return (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2
+
+
+def F_9_i(x):
+    return (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2
+
+
+# Функция Изома
+def F_10(x):
+    return -math.cos(x[0]) * math.cos(x[1]) * math.exp(-((x[0] - math.pi) ** 2 + (x[1] - math.pi) ** 2))
+
+
+def F_10_i(x):
+    return -imath.cos(x[0]) * imath.cos(x[1]) * imath.exp(-((x[0] - math.pi) ** 2 + (x[1] - math.pi) ** 2))
+
+
+# Функция Шафера
+def F_11(x):
+    return 0.5 + (imath.sin(x[0] ** 2 - x[1] ** 2) ** 2 - 0.5) / (1 + 0.001 * (x[0] ** 2 + x[1] ** 2)) ** 2
+
+
+def F_11_i(x):
+    return 0.5 + (imath.sin(x[0] ** 2 - x[1] ** 2) ** 2 - 0.5) / (1 + 0.001 * (x[0] ** 2 + x[1] ** 2)) ** 2
+
+
+# Функция Стыбинского-Танга
+def F_12(x):
+    n = len(x)
+    val = 0
+    for i in range(n):
+        val += x[i] ** 4 - 16 * x[i] ** 2 + 5 * x[i]
+    return val / 2
+
+
+def F_12_i(x):
+    n = len(x)
+    val = 0
+    for i in range(n):
+        val += x[i] ** 4 - 16 * x[i] ** 2 + 5 * x[i]
+    return val / 2
+
+
+Functions = [F_0, F_0_i,
+             F_1, F_1_i,
+             F_2, F_2_i,
+             F_3, F_3_i,
+             F_4, F_4_i,
+             F_5, F_5_i,
+             F_6, F_6_i,
+             F_7, F_7_i,
+             F_8, F_8_i,
+             F_9, F_9_i,
+             F_10, F_10_i,
+             F_11, F_11_i,
+             F_12, F_12_i]
