f02663d8589fdb2bfdc4081de6bbefb891b00aff
sudakovcom <ilyasudakov@icloud.com>
2023-05-15 22:30:34 +0300

    added M-S gradient

--- coursework.py
+++ coursework.py
@@ -164,7 +164,7 @@ def FastSearch(func_index, D, p, e_d, e_f,
                        e_f)
         if (dist(p_0, p) < e_d) & (
                 Functions[func_index * 2](p_0) - Functions[func_index * 2](
-                p) < e_f):
+            p) < e_f):
             break
 
     best_point = p
@@ -245,61 +245,76 @@ def GoldenRatioG(func_index, gradient, D, p, e_d,
 
     return best_point  # point of extremum with error e_d
 
-# def MooreSkelboeG(func_index, gradient, D, p, e_d, e_f):  # F(function), i(index of direction),
-#     # a(left border), b(right border), p(current point), e_d(error of d), e_f(error of f)
-#     F = Functions[func_index * 2 + 1]
-#     interval_d = []
-#     for i in range(len(p)):
-#         interval_d.append(interval[p[i], p[i]])
-#     interval_d[index] = interval[a, b]
-#
-#     interval_f = F(interval_d)
-#     set_of_intervals = [[interval_d, interval_f]]
-#     U = right(interval_f)
-#     w_f = right(interval_f) - left(interval_f)
-#     w_d = right(interval_d[index]) - left(interval_d[index])
-#     best_interval = set_of_intervals[0]
-#     while (w_d > e_d) | (w_f > e_f):
-#         set_of_intervals.pop(0)
-#         mid_p = mid(best_interval[0][index])
-#         interval_1 = best_interval[0].copy()
-#         interval_2 = best_interval[0].copy()
-#         interval_1[index] = interval[left(best_interval[0][index]), mid_p]
-#         interval_1_f = F(interval_1)
-#         interval_2[index] = interval[mid_p, right(best_interval[0][index])]
-#         interval_2_f = F(interval_2)
-#         U = min(U, right(interval_1_f))
-#         U = min(U, right(interval_2_f))
-#
-#         for i in range(len(set_of_intervals)):
-#             if U < left(set_of_intervals[i][1]):
-#                 set_of_intervals = set_of_intervals[:i]
-#                 break
-#
-#         set_of_intervals.append([interval_1, interval_1_f])
-#         set_of_intervals.append([interval_2, interval_2_f])
-#
-#         set_of_intervals.sort(key=lambda item: left(item[1]))
-#         best_interval = set_of_intervals[0]
-#         w_f = right(best_interval[1]) - left(best_interval[1])
-#         w_d = right(best_interval[0][index]) - left(best_interval[0][index])
-#         print(w_f)
-#
-#     best_point = []
-#     for i in range(len(p)):
-#         best_point.append(mid(best_interval[0][i]))
-#
-#     return best_point
+
+def MooreSkelboeG(func_index, gradient, D, p, e_d,
+                  e_f):  # p(current point), e_d(error of d), e_f(error of f)
+    F = Functions[func_index * 2 + 1]
+    interval_d = [None] * len(p)
+
+    a = Borders(p, gradient, D)[0]
+    b = Borders(p, gradient, D)[1]
+
+    for i in range(len(p)):
+        interval_d[i] = interval[a[i], b[i]]
+
+    interval_f = F(interval_d)
+    set_of_intervals = [[interval_d, interval_f]]
+    U = right(interval_f)
+    w_f = right(interval_f) - left(interval_f)
+    for index in range(len(p)):
+        w_d = np.square(right(interval_d[index]) - left(interval_d[index]))
+    w_d = np.sqrt(w_d)
+    best_interval = set_of_intervals[0]
+    while (w_d > e_d) | (w_f > e_f):
+        set_of_intervals.pop(0)
+        mid_p = [None] * len(p)
+        for index in range(len(p)):
+            mid_p[index] = mid(best_interval[0][index])
+
+        interval_1 = best_interval[0].copy()
+        interval_2 = best_interval[0].copy()
+        for index in range(len(p)):
+            interval_1[index] = interval[left(best_interval[0][index]), mid_p[index]]
+            interval_2[index] = interval[mid_p[index], right(best_interval[0][index])]
+
+        interval_1_f = F(interval_1)
+        interval_2_f = F(interval_2)
+
+        U = min(U, right(interval_1_f))
+        U = min(U, right(interval_2_f))
+
+        for i in range(len(set_of_intervals)):
+            if U < left(set_of_intervals[i][1]):
+                set_of_intervals = set_of_intervals[:i]
+                break
+
+        set_of_intervals.append([interval_1, interval_1_f])
+        set_of_intervals.append([interval_2, interval_2_f])
+
+        set_of_intervals.sort(key=lambda item: left(item[1]))
+        best_interval = set_of_intervals[0]
+        w_f = right(best_interval[1]) - left(best_interval[1])
+
+        for index in range(len(p)):
+            w_d = np.square(right(best_interval[0][index]) - left(best_interval[0][index]))
+        w_d = np.sqrt(w_d)
+        # print(w_f)
+
+    best_point = []
+    for i in range(len(p)):
+        best_point.append(mid(best_interval[0][i]))
+
+    return best_point
 
 
 # print(FastSearchG(3, [[-10, 10], [-10, 10]], [1, 1], 0.01, 0.01, GoldenRatioG))
-
 
 n = 2
 l = -500
 r = 500
 D = [[l, r]] * n
 p = [2] * n
+print(FastSearchG(18, D, p, 0.001, 0.001, MooreSkelboeG)[1])
 print(FastSearchG(18, D, p, 0.001, 0.001, GoldenRatioG)[1])
 
 print("finished")
